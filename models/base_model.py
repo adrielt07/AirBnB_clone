0f54319dfef4484e081145b06218c1a06a7bce0a
Adriel Tolentino <adrieltolentino@outlook.com>
2018-06-07 20:50:24 +0000

    added docstring

--- models/base_model.py
+++ models/base_model.py
@@ -9,15 +9,26 @@ class BaseModel:
     BaseModel Class
     """
     def __init__(self):
+        """
+        creates uuid specific for each instance
+        """
         import uuid
         self.id = str(uuid.uuid4())
         self.created_at = datetime.datetime.today()
         self.updated_at = datetime.datetime.today()
 
     def save(self):
+        """
+        updates time - last time instance object is modified
+        """
         self.updated_at = datetime.datetime.today()
 
     def to_dict(self):
+        """
+        returns a new copy of dictionary
+        added '__class__' key
+        Updated time to isoformat
+        """
         from datetime import datetime, date, time
         new = self.__dict__.copy()
         new['__class__'] = type(self).__name__
@@ -26,5 +37,8 @@ class BaseModel:
         return new
 
     def __str__(self):
+        """
+        Prints Classname, instance id, and dictionary
+        """
         return "[{}] ({}) {}".format(type(self).__name__,
                                      self.id, self.__dict__)
