f90b226ed7f60aa1aef1a16cf5fb3f3caa08f447
Adriel Tolentino <adrieltolentino@outlook.com>
2018-06-07 20:31:22 +0000

    change the created_at and updated_at value

--- models/base_model.py
+++ models/base_model.py
@@ -11,15 +11,18 @@ class BaseModel:
     def __init__(self):
         import uuid
         self.id = str(uuid.uuid4())
-        self.created_at = datetime.date.today()
-        self.updated_at = datetime.date.today()
+        self.created_at = datetime.datetime.today()
+        self.updated_at = datetime.datetime.today()
 
     def save(self):
-        self.update_at = datetime.date.today()
+        self.update_at = datetime.datetime.today()
 
     def to_dict(self):
+        from datetime import datetime, date, time
         new = self.__dict__.copy()
         new['__class__'] = type(self).__name__
+        new['created_at'] = datetime.isoformat(self.created_at)
+        new['updated_at'] = datetime.isoformat(self.updated_at)
         return new
 
     def __str__(self):
