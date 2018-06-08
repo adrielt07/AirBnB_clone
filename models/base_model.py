50181a37f37ca8f2fac08aaab4284c44ffc9588a
adrielt07 <adriel_tolentino@outlook.com>
2018-06-08 01:58:10 +0000

    import datetime in __init__ method to shorten its name, add kwargs to create a method to generate a dictionary representation of an instance

--- models/base_model.py
+++ models/base_model.py
@@ -2,7 +2,6 @@
 """
 BaseModel that defines all common attributes/methods for other classes
 """
-import datetime
 
 
 class BaseModel:
@@ -13,10 +12,20 @@ class BaseModel:
         """
         creates uuid specific for each instance
         """
+        from datetime import datetime
         import uuid
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.datetime.today()
-        self.updated_at = datetime.datetime.today()
+
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    setattr(self, key,
+                            datetime.strptime(value,"%Y-%m-%dT%H:%M:%S.%f"))
+                if key != '__class__':
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.today()
+            self.updated_at = datetime.today()
 
     def save(self):
         """updates time - last time instance object is modified"""
