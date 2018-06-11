ef0bf205260b956d67a9750819ddfd682c6d2128
Amy <amy.tai0120@gmail.com>
2018-06-11 17:43:23 +0000

    update __init__ with all classes

--- models/__init__.py
+++ models/__init__.py
@@ -11,7 +11,9 @@ storage.reload()
 if __name__ != '__main__':
     def error_check(list_arg=[], n=0):
         """Check for common error messages for all methods"""
-        check_class = ['BaseModel', 'User']
+        check_class = ['BaseModel', 'User', 'State',
+                       'City', 'Amenity', 'Place', 'Review']
+
         try:
             class_name = "{}".format(list_arg[0])
         except IndexError:
